827f5e8e6c66a733764a1c6dcdce07772b7f7a45
bobi1995 <borislav.stefanov.1995@gmail.com>
2025-04-07 14:32:02 +0300

    Connect backend to frontend with apollo/client and add first query

---
+++ src/db/config.ts
@@ -0,0 +1,4 @@
+export const dev_graphqlEndpoint = "http://localhost:3001/graphql";
+export const dev_endpoint = "http://localhost:3001";
+export const prod_graphqlEndpoint = "https://api.example.com/graphql";
+export const prod_endpoint = "https://api.example.com";
---
+++ src/page/Users.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useGetUser } from "../graphql/hooks/user";
+
+const Users = () => {
+  const { users, loading, error, refetch } = useGetUser("", 10, 0);
+
+  if (loading) return <p>Loading...</p>;
+  if (error)
+    return (
+      <div>
+        ERORR!:
+        <button
+          onClick={() => {
+            console.log("refetching...");
+            refetch();
+          }}
+          className="text-black"
+        >
+          Reload
+        </button>
+      </div>
+    );
+
+  console.log(users, loading, error);
+  return <div>{users.getAllUsers.map((el: any) => `${el.name}\n`)}</div>;
+};
+
+export default Users;

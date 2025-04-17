be047debeb58f8681bcc4540e43483cdc5f6f98f
Kaloyan Krastev <kaimikan@protonmail.com>
2025-04-17 14:51:33 +0300

    Add group card for stats

--- src/page/UserManagementPage.tsx
+++ src/page/UserManagementPage.tsx
@@ -10,6 +10,7 @@ import { CreateUserInput, UpdateUserInput } from "../graphql/mutation/user";
 import CreateUserModal from "../components/modals/CreateUserModal";
 import CreateUserFormModal from "../components/modals/CreateUserFormModal";
 import { useGetRoles } from "../graphql/hooks/role";
+import RoleStatsCards from "../components/cards/RoleStatsCard";
 
 export interface Role {
   __typename?: "Role";
@@ -158,35 +159,11 @@ const UserManagementPage: React.FC = () => {
       {/* Stats and Actions Section */}
       <section className="mb-6 flex flex-wrap items-start justify-between gap-4">
         {/* Stats Cards - Updated counts */}
-        <div className="flex flex-wrap gap-4">
-          <div className="flex min-w-[200px] items-center space-x-3 rounded-md border border-gray-200 bg-white p-4 shadow-sm">
-            <UsersIcon className="h-8 w-8 text-gray-400" />
-            <div>
-              <p className="text-xs text-gray-500">Брой потребители</p>
-              <p className="text-2xl font-semibold text-gray-800">
-                {totalUsers}
-              </p>
-            </div>
-          </div>
-          <div className="flex min-w-[200px] items-center space-x-3 rounded-md border border-gray-200 bg-white p-4 shadow-sm">
-            <div className="w-[32px]"></div>
-            <div>
-              <p className="text-xs text-gray-500">Експерти</p>
-              <p className="text-2xl font-semibold text-gray-800">
-                {expertCount}
-              </p>
-            </div>
-          </div>
-          <div className="flex min-w-[200px] items-center space-x-3 rounded-md border border-gray-200 bg-white p-4 shadow-sm">
-            <div className="w-[32px]"></div>
-            <div>
-              <p className="text-xs text-gray-500">Администратори</p>
-              <p className="text-2xl font-semibold text-gray-800">
-                {adminCount}
-              </p>
-            </div>
-          </div>
-        </div>
+        <RoleStatsCards
+          totalUsers={totalUsers}
+          expertCount={expertCount}
+          adminCount={adminCount}
+        />
 
         {/* Create User Button */}
         <button

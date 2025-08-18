74bdad441c874d028c87567468adbf5196094a33
bobi1995 <borislav.stefanov.1995@gmail.com>
2025-08-18 11:12:58 +0300

    Fix notification translation after Server's changes

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,5 +1,8 @@
 // export const endpoint = "http://192.168.1.152:3001";
 // export const graphqlEndpoint = "http://192.168.1.152:3001/graphql";
 
+// export const endpoint = "http://192.168.1.202:3002";
+// export const graphqlEndpoint = "http://192.168.1.202:3002/graphql";
+
 export const graphqlEndpoint = "http://localhost:3001/graphql";
 export const endpoint = "http://localhost:3001";
